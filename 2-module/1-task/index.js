16095ce9b7d3e21ad057b89bfa655fd3241d47a2
StasVladimirov <68865193+StasVladimirov@users.noreply.github.com>
2020-08-05 20:13:40 +0300

    Update index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,12 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(salaries){
+  let sum = 0;
+  for (let key in salaries){
+    if (typeof (salaries[key]) == 'number'){
+    sum += salaries[key]; 
+   }
+  }
+  return sum
 }
