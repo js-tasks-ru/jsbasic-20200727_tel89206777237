e86d4e4d9e062dbb8bafe8ec6c0648bb79878366
StasVladimirov <68865193+StasVladimirov@users.noreply.github.com>
2020-08-05 20:16:45 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,15 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
-  // ваш код...
+function factorial(n){
+  if(n==0) {
+    return 1
+  } else {
+   let a = n;
+   while(a>1){
+   a--;
+   n*=a;
+    
+  }
+   return n}
 }
